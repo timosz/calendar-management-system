ec979a0a67413fab49916ea11511b505af324d2a
Timos Zabetakis <timoszab@gmail.com>
2025-10-08 15:43:47 +0300

    refactors restrictions controller

--- app/Http/Controllers/Admin/RestrictionController.php
+++ app/Http/Controllers/Admin/RestrictionController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Actions\Restriction\GetRestrictionsAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StoreRestrictionRequest;
 use App\Http\Requests\Admin\UpdateRestrictionRequest;
+use App\Http\Resources\RestrictionFormResource;
+use App\Http\Resources\RestrictionResource;
 use App\Models\Restriction;
 use App\Services\TimeSlotService;
 use Illuminate\Http\Request;
@@ -12,61 +15,30 @@ use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Http\RedirectResponse;
-use Carbon\Carbon;
 
 class RestrictionController extends Controller
 {
     public function __construct(
-        protected TimeSlotService $timeSlotService
+        protected TimeSlotService $timeSlotService,
+        protected GetRestrictionsAction $getRestrictionsAction
     ) {
     }
 
     public function index(Request $request): Response
     {
-        $query = Auth::user()->restrictions();
+        $filters = [
+            'type' => $request->type,
+            'from_date' => $request->from_date,
+            'to_date' => $request->to_date,
+        ];
 
-        // Filter by type if specified
-        if ($request->filled('type')) {
-            $query->ofType($request->type);
-        }
-
-        // Filter by date range if specified
-        if ($request->filled('from_date')) {
-            $fromDate = Carbon::parse($request->from_date);
-            $query->where('end_date', '>=', $fromDate);
-        }
-
-        if ($request->filled('to_date')) {
-            $toDate = Carbon::parse($request->to_date);
-            $query->where('start_date', '<=', $toDate);
-        }
-
-        $restrictions = $query
-            ->orderBy('start_date')
-            ->orderBy('start_time')
-            ->paginate(15)
-            ->through(function ($restriction) {
-                return [
-                    'id' => $restriction->id,
-                    'start_date' => $restriction->start_date->format('Y-m-d'),
-                    'end_date' => $restriction->end_date->format('Y-m-d'),
-                    'start_time' => $restriction->start_time,
-                    'end_time' => $restriction->end_time,
-                    'is_all_day' => $restriction->isAllDay(),
-                    'reason' => $restriction->reason,
-                    'type' => $restriction->type,
-                    'type_label' => Restriction::getTypes()[$restriction->type],
-                ];
-            });
+        $restrictions = $this->getRestrictionsAction
+            ->execute(Auth::user(), array_filter($filters));
 
         return Inertia::render('Admin/Restrictions/Index', [
-            'restrictions' => $restrictions,
+            'restrictions' => RestrictionResource::collection($restrictions),
             'types' => Restriction::getTypes(),
-            'filters' => [
-                'type' => $request->type,
-                'from_date' => $request->from_date,
-                'to_date' => $request->to_date,
-            ],
+            'filters' => $filters,
         ]);
     }
 
@@ -90,15 +62,7 @@ class RestrictionController extends Controller
     public function edit(Restriction $restriction): Response
     {
         return Inertia::render('Admin/Restrictions/Edit', [
-            'restriction' => [
-                'id' => $restriction->id,
-                'start_date' => $restriction->start_date->format('Y-m-d'),
-                'end_date' => $restriction->end_date->format('Y-m-d'),
-                'start_time' => $restriction->start_time ? substr($restriction->start_time, 0, 5) : null,
-                'end_time' => $restriction->end_time ? substr($restriction->end_time, 0, 5) : null,
-                'reason' => $restriction->reason,
-                'type' => $restriction->type,
-            ],
+            'restriction' => new RestrictionFormResource($restriction),
             'types' => Restriction::getTypes(),
             'timeSlots' => $this->timeSlotService->generateTimeOptions(30),
         ]);
